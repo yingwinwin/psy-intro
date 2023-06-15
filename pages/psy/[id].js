92d02859bb7717d25b78193644b8c791746e25a7
周赢 <15204671021@163.com>
2023-06-15 20:41:47 +0800

    rollback

--- pages/psy/[id].js
+++ pages/psy/[id].js
@@ -18,7 +18,7 @@ export async function getStaticPaths() {
     const paths = await getAllPsyIds();
     return {
         paths,
-        fallback: true,
+        fallback: false,
     };
 }
 export default function Post({ psyData }) {
